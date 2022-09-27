7095fa639c587099ff68f28acab61e167ef6de7b
kan.kosal <kan.kosal@sabay.com>
2022-09-27 15:05:46 +0700

    publish database migration

--- src/UserServiceProvider.php
+++ src/UserServiceProvider.php
@@ -13,7 +13,18 @@ class UserServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'blogpackage');
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/config/config.php' => config_path('admin_user.php'),
+            ], 'config');
+
+            if (! class_exists('CreateUsersTable')) {
+                $this->publishes([
+                  __DIR__ . '/database/migrations/2014_10_12_000000_create_users_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_users_table.php'),
+                  // you can add any number of migrations here
+                ], 'migrations');
+            }
+        }
     }
 
     /**
